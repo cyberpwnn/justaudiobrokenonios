6d300cb511162dc4e1a79172ad4b430fcf12d0a4
Daniel Mills <danielmillst@gmail.com>
2021-01-21 21:59:33 -0500

    Minrepro

---
+++ android/app/src/main/kotlin/com/example/justaudiobrokenonios/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.justaudiobrokenonios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _play() {
+    AudioPlayer p = AudioPlayer();
+    p.setUrl("https://icarus-cdn.nyc3.cdn.digitaloceanspaces.com/resources/_6/7b/e2EC/D9C94F85aca9bce1/A75e2CfBd5D7/48B53b83de64/9FB07A4b936a69f4/de6Fab25/e01a1388FC970Ac7").then((value) => p.load().then((value) => p.play().then((value) => p.stop().then((value) => p.dispose()))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _play,
+        tooltip: 'Play',
+        child: Icon(Icons.play_arrow),
+      ), 
+    );
+  }
+}
