ca07dd140fed47a5a1fe1a14cea1e470c15423df
Daniel Mills <danielmillst@gmail.com>
2021-01-21 22:21:38 -0500

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'JustAudio Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'JustAudio Demo'),
     );
   }
 }
@@ -42,7 +32,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _play() {
     AudioPlayer p = AudioPlayer();
-    p.setUrl("https://icarus-cdn.nyc3.cdn.digitaloceanspaces.com/resources/_6/7b/e2EC/D9C94F85aca9bce1/A75e2CfBd5D7/48B53b83de64/9FB07A4b936a69f4/de6Fab25/e01a1388FC970Ac7").then((value) => p.load().then((value) => p.play().then((value) => p.stop().then((value) => p.dispose()))));
+    p.setUrl("https://icarus-cdn.nyc3.cdn.digitaloceanspaces.com/resources/_6/7b/e2EC/D9C94F85aca9bce1/A75e2CfBd5D7/48B53b83de64/9FB07A4b936a69f4/de6Fab25/e01a1388FC970Ac7").then((value) =>
+     p.load().then((value) => 
+     p.play().then((value) =>
+     p.stop().then((value) => 
+     p.dispose()))));
   }
 
   @override
